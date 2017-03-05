5bf4ca2d350d0370b9f31ec9553d934c8342215c
Alex Perez <aperrez17@gmail.com>
2017-03-05 11:44:36 -0500

    Setup ES create index path

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,9 @@ import Keys._
 
 object Dependencies {
   val playElastic = "com.evojam" %% "play-elastic4s" % "0.3.1"
+  val guice = "com.google.inject" % "guice" % "3.0"
   val elasticDependencies: Seq[ModuleID] = Seq(
-    playElastic
+    playElastic,
+    guice
   )
 }
